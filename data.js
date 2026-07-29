f06a842ce451f61b136b66eaa691f7dc45f6252a
Tanaya Bedase <115339691+tayvol@users.noreply.github.com>
2026-07-29 05:34:23 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,147 @@
+const users = [
+
+{
+    id:1,
+
+    name:"Ana",
+
+    age:22,
+
+    city:"Mumbai",
+
+    gender:"Female",
+
+    income:100000,
+
+    likes:["Cycling","Coffee","Music"],
+
+    dislikes:["Getting Late"],
+
+    hobbies:["Reading","Photography"],
+
+    bio:"Adventure lover who enjoys long drives, coffee dates and exploring new places.",
+
+    image:"https://randomuser.me/api/portraits/women/44.jpg"
+},
+
+{
+    id:2,
+
+    name:"Diya",
+
+    age:22,
+
+    city:"New Delhi",
+
+    gender:"Female",
+
+    income:300000,
+
+    likes:["Music","Travel"],
+
+    dislikes:["Workout"],
+
+    hobbies:["Paragliding","Reading"],
+
+    bio:"Music enthusiast who loves travelling and discovering hidden cafés.",
+
+    image:"https://randomuser.me/api/portraits/women/68.jpg"
+},
+
+{
+    id:3,
+
+    name:"Ian",
+
+    age:22,
+
+    city:"Bengaluru",
+
+    gender:"Male",
+
+    income:10000,
+
+    likes:["Dancing","Travel"],
+
+    dislikes:["Smoking"],
+
+    hobbies:["Painting","Gaming"],
+
+    bio:"Creative artist who enjoys painting and dancing every weekend.",
+
+    image:"https://randomuser.me/api/portraits/men/32.jpg"
+},
+
+{
+    id:4,
+
+    name:"Jack",
+
+    age:22,
+
+    city:"Mumbai",
+
+    gender:"Male",
+
+    income:10000,
+
+    likes:["Skating","Fitness"],
+
+    dislikes:["Travel"],
+
+    hobbies:["Wood Working","Cycling"],
+
+    bio:"Engineer by profession, skater by passion.",
+
+    image:"https://randomuser.me/api/portraits/men/40.jpg"
+},
+
+{
+    id:5,
+
+    name:"Sam",
+
+    age:22,
+
+    city:"Bengaluru",
+
+    gender:"Male",
+
+    income:10000,
+
+    likes:["Food","Coding"],
+
+    dislikes:["Dance"],
+
+    hobbies:["Coding","Movies"],
+
+    bio:"Software developer who can spend hours coding and eating pizza.",
+
+    image:"https://randomuser.me/api/portraits/men/58.jpg"
+},
+
+{
+    id:6,
+
+    name:"Nick",
+
+    age:22,
+
+    city:"Mumbai",
+
+    gender:"Male",
+
+    income:10000,
+
+    likes:["Skating","Photography"],
+
+    dislikes:["Travel"],
+
+    hobbies:["Wood Working","Football"],
+
+    bio:"Always looking for someone to join spontaneous adventures.",
+
+    image:"https://randomuser.me/api/portraits/men/75.jpg"
+}
+
+];
